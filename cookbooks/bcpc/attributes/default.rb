d1f94a20ab835436ca9efb5ad7ca13590540f84a
Pravir Chandra <chandra@list.org>
2014-08-11 01:10:45 -0400

    clean-up vars and other remainders from nova-network

--- cookbooks/bcpc/attributes/default.rb
+++ cookbooks/bcpc/attributes/default.rb
@@ -58,7 +58,6 @@ default['bcpc']['ceph']['enabled_pools'] = ["ssd", "hdd"]
 default['bcpc']['management']['interface'] = "eth0"
 default['bcpc']['storage']['interface'] = "eth1"
 default['bcpc']['floating']['interface'] = "eth2"
-default['bcpc']['fixed']['vlan_interface'] = node['bcpc']['floating']['interface']
 
 ###########################################
 #
@@ -101,7 +100,6 @@ default['bcpc']['management']['vip'] = "10.17.1.15"
 default['bcpc']['management']['netmask'] = "255.255.255.0"
 default['bcpc']['management']['cidr'] = "10.17.1.0/24"
 default['bcpc']['management']['gateway'] = "10.17.1.1"
-default['bcpc']['metadata']['ip'] = "169.254.169.254"
 
 default['bcpc']['storage']['netmask'] = "255.255.255.0"
 default['bcpc']['storage']['cidr'] = "100.100.0.0/24"
@@ -111,13 +109,6 @@ default['bcpc']['floating']['vip'] = "192.168.43.15"
 default['bcpc']['floating']['netmask'] = "255.255.255.0"
 default['bcpc']['floating']['cidr'] = "192.168.43.0/24"
 default['bcpc']['floating']['gateway'] = "192.168.43.2"
-default['bcpc']['floating']['available_subnet'] = "192.168.43.128/25"
-
-default['bcpc']['fixed']['cidr'] = "1.127.0.0/16"
-default['bcpc']['fixed']['vlan_start'] = "1000"
-default['bcpc']['fixed']['num_networks'] = "100"
-default['bcpc']['fixed']['network_size'] = "256"
-default['bcpc']['fixed']['dhcp_lease_time'] = 3600
 
 default['bcpc']['ntp_servers'] = ["pool.ntp.org"]
 default['bcpc']['dns_servers'] = ["8.8.8.8", "8.8.4.4"]
